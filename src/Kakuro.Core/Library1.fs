93f81014d7a018b8c5d7fd4644835adbc0a6ba18
Maximo Dominguez <mamodom@gmail.com>
2015-08-16 17:35:16 -0400

    Playing Arround

---
+++ src/Kakuro.Core/Library1.fs
@@ -0,0 +1,26 @@
+﻿namespace Kakuro.Core
+
+type Dimension(height: int, width:int) =
+    member this.Height = height
+    member this.Width = width
+
+type Board(size: Dimension ) = 
+    member this.Height = size.Height
+    member this.Width = size.Width
+    
+type Sums(downSum:int option, rightSum:int option) =
+    member this.DownSum = downSum
+    member this.RightSum = rightSum
+
+type Content = 
+    |Empty
+    |Value of int
+    |Sum of Sums
+
+type Element(content: Content) =
+    let Content = content
+    
+module  Foo =
+    let LoadBoard  (path:string) = 
+        System.IO.File.ReadLines(path)
+        |> Seq.iter(fun x -> printfn  "%s" x) 
