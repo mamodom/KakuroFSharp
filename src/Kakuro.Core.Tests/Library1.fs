51f88bee5e64d7c41ccf3109beb11c3388cfdbe0
Maximo Dominguez <mamodom@gmail.com>
2015-08-16 22:24:56 -0400

    Adding tests for existing functionality

--- src/Kakuro.Core.Tests/Library1.fs
+++ src/Kakuro.Core.Tests/Library1.fs
@@ -6,7 +6,33 @@ module ``Kakuro cell parser`` =
     open Kakuro.Core.Parser
 
     [<Test>]
-    let `` parses an empty sum cell`` () =
-        (parseCell "\\" ).ToString() 
-        |> should equal "X"
-        
+    let ``parses an empty sum cell`` () =
+        parseCell "\\"
+        |> should equal (Sums(None, None))
+    
+    [<Test>]
+    let ``parses a sum cell without column sum`` () =
+        parseCell "\\45"
+        |> should equal (Sums(None, Some(45)))
+    
+    [<Test>]
+    let ``parses a sum cell without row sum`` () =
+        parseCell "45\\"
+        |> should equal (Sums(Some(45), None)) 
+    
+    [<Test>]
+    let ``parses a sum cell`` () =
+        parseCell "45\\34"
+        |> should equal (Sums(Some(45), Some(34)))
+    
+    [<Test>]
+    let ``parses an empty value cell`` () =
+        parseCell ""
+        |> should equal (Value(None)) 
+    
+    [<Test>]
+    let ``parses a value cell`` () =
+        parseCell "15"
+        |> should equal (Value(Some(15)))
+    
+    
